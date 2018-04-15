09b6d2175496d4d7d8562d057a0da15ba5ab24bc
Muataz Khataan <muataz.khataan@gmail.com>
2018-04-15 08:49:10 +0300

    Muataz

---
+++ Assets/Scripts/mutant_Animation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mutant_Animation : MonoBehaviour {
+
+	public Animation anim;
+
+	IEnumerator Start() {
+		anim = GetComponent<Animation> ();
+		anim.Play (anim.clip.name);
+		yield return new WaitForSeconds (anim.clip.length);
+	}
+
+}
